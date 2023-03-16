86e25c14852b73533bf10c84ab746205ee54dca8
seonghtun <74886046+seonghtun@users.noreply.github.com>
2023-03-16 14:38:50 +0900

    add age.c lib dir in myheader.h

--- c/gcc/age.c
+++ c/gcc/age.c
@@ -1,11 +1,6 @@
 #include <stdio.h>
+#include "myheader.h"
 
-int main() {
-  int i;
-
-  printf("Input your age : ");
-  scanf("%d", &i);// address of i call by adderess method
-  printf("Your age is %d \n", i);// call by value method
-
-  return 0;// 프로그램 꺼지니까 0 을 리턴
+void main() {
+  printf("Your age is %d \n", AGE);// call by value method
 }
