019b9d9d086330eb64bdb3d18bfaf5c836913437
seonghtun <ysh970718@gmail.com>
2023-04-13 11:06:35 +0900

    fix user html and css and main.js function upgrade

--- node/member/routes/main.js
+++ node/member/routes/main.js
@@ -53,9 +53,44 @@ app.post('/login', (req, res) => {
 
 app.post('/register', (req, res) => {
     const { id, pw } = req.body;
-    const result = connection.query("insert into user values (?,?)", [id, pw]);
-    console.log(result);
-    res.redirect('/');
+    if (id === "") {
+        res.redirect("/");
+        console.log("insert to ID")
+    }
+    else {
+        let result = connection.query('select * from user where userid=?', [id]);
+        if (result[0].userid === id) {
+            res.writeHead(200);
+            var template = `
+            <!DOCTYPE html>
+            <html>
+
+            <head>
+                <meta charset="utf-8">
+                <link href="mystyle.css" type="text/css" rel="stylesheet">
+                <title>
+                    Error
+                </title>
+            </head>
+
+            <body>
+                <div>
+                    <h3 style="margin-left:30px">Register Failed</h3>
+                    <h4 style="margin-left:30px">이미 존재하는 아이디입니다</h4>
+                    
+                    <a href="register.html" style="margin-left:30px">다시 시도하기</a>
+                </div>
+            </body>
+
+            </html>
+            `;
+            res.end(template);
+        } else {
+            const result = connection.query("insert into user values (?,?)", [id, pw]);
+            console.log(result);
+            res.redirect('/');
+        }
+    }
 })
 
 module.exports = app;
