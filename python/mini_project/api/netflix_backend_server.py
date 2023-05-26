31314a347b90f19f7d97eb415177265559b7c567
seonghtun <ysh970718@gmail.com>
2023-05-26 20:21:22 +0900

    Feat netflix graph and data api

--- python/mini_project/api/netflix_backend_server.py
+++ python/mini_project/api/netflix_backend_server.py
@@ -7,16 +7,34 @@ from bson.objectid import ObjectId
 import json
 import os.path
 from datetime import datetime, timedelta
+from pydantic import BaseModel
+import pydantic
+import bson
+from bson.objectid import ObjectId
+import matplotlib.pyplot as plt
+
+BASE_DIR = os.path.dirname(os.path.relpath("./"))
+GRAPH_DIR = "../graph/"
+secret_file = os.path.join(BASE_DIR, "../../secret.json")
 
 pd.set_option('display.max_columns',10)
 pd.set_option('display.max_rows',50)
 
-# pydantic.json.ENCODERS_BY_TYPE[objectid] = str
+pydantic.json.ENCODERS_BY_TYPE[ObjectId] = str
+
+class GraphElements(BaseModel):
+    title:str 
+    countries:list = []
+
+class Netflix(BaseModel):
+    title: str
+    period_start: str
+    period_end: str
+    
 
 app = FastAPI()
 
-BASE_DIR = os.path.dirname(os.path.relpath("./"))
-secret_file = os.path.join(BASE_DIR, "../../secret.json")
+
 
 with open(secret_file) as f:
     secrets = json.loads(f.read())
@@ -30,27 +48,32 @@ def get_secret(setting, secrets=secrets):
         return errorMsg
 
 MONGO_HOST = get_secret('Mongo_Host')
-JSON_HOST = get_secret('JSON_Host')
+JSON_HOST = get_secret('Netflix_JSON_Host')
+
+user_list=["Squid Game", "All of Us Are Dead","Hometown Cha-Cha-Cha", "The Glory"]
 
 client = mongo_client.MongoClient(f'{MONGO_HOST}')
 
 mydb = client['test']
-mycol = mydb['netflix']
+total_col = mydb['total_netflix']
+period_col = mydb['period_netflix']
+graph_col = mydb['graphs']
+
 
 print('Connected to Mongodb.....')
 
-def getAPI(countries=None,title=None,url = JSON_HOST):
+
+def getAPI(countries=None,title=None,url = JSON_HOST, period=None):
     url += '?'
     if countries != None:
-        
         for i in range(len(countries)):
             url += 'country=' + countries[i]+'&'
-        url = url[:-1]
     if title==None:
         return print("제목은 들어와야 됩니다.")
-    url += 'title='+title
-    response = requests.get(url)
-    print(type(response.text))
+    if period!=None:
+        url += f"week_gte={period[0]}&week_lte={period[1]}&"
+    url += 'show_title='+title
+    response =  requests.get(url)
     _dict = json.loads(response.text)
     return _dict
 
@@ -71,18 +94,107 @@ def mongo_delete():
     else:
         return result
 
+
+
 @app.get('/')
 async def hello():
     return 'Hello yoon'
 
-@app.get(path='/netflix_country')
-async def country(title = None):
-    print(countries, title)
-    result = getAPI(show_title=title)
+@app.get(path='/netflix_total_country')
+async def country(title):
+    result = getAPI(title=user_list[int(title)])
     if len(result) == 0:
         return "데이터가 없습니다."
-    return result
+    total_col.insert_many(result)
+    # 정렬된 top10 누적주수 제일 많은 나라 순 정렬해서
 
+    df = pd.DataFrame(result)
+    df['cumulative_weeks_in_top_10'] = df['cumulative_weeks_in_top_10'].astype(int)
+
+    order_dict = df.loc[df.groupby('country_name')['cumulative_weeks_in_top_10'].idxmax()]\
+                        .sort_values(['cumulative_weeks_in_top_10'],ascending=False)\
+                        .to_dict(orient='records')
+    print(type(order_dict))
+    return order_dict
+
+@app.post(path='/netflix_period_country')
+async def netflix_period(request:Netflix):
+    print(request.period_start)
+    start = datetime.strptime(request.period_start,"%Y-%m").date()
+    end = datetime.strptime(request.period_end,"%Y-%m").date()
+    result = getAPI(title=user_list[int(request.title)], period=[start,end])
+
+    # 정렬된 top10 누적주수 제일 많은 나라 순 정렬해서
+    df = pd.DataFrame(result)
+    df['cumulative_weeks_in_top_10'] = df['cumulative_weeks_in_top_10'].astype(int)
+
+    order_dict = df.loc[df.groupby('country_name')['cumulative_weeks_in_top_10'].idxmax()]\
+                        .sort_values(['cumulative_weeks_in_top_10'],ascending=False)\
+                        .to_dict(orient='records')
+    return order_dict
+
+@app.post(path='/total_graph')
+async def make_graph(req : GraphElements):
+    print(req.title, req.countries)
+    graphs = []
+    data = total_col.find({'country_name':{'$in':req.countries}},{"_id":0})
+    filename = 'Graph01.png'
+    
+    countries_rank_graph(data,req.countries,GRAPH_DIR + filename,user_list[int(req.title)])
+    with open(GRAPH_DIR + filename, 'rb') as f:
+        graphs.append({"filename":filename, "imagefile" : f.read()})    
+    
+    data = total_col.find({'$and':[{'country_name':{'$in':req.countries}},{'weekly_rank':'1'}]},{"_id":0})
+    filename = "Graph02.png"
+    first_count_graph(data, GRAPH_DIR + filename,user_list[int(req.title)]+ ' 1st')
+    with open(GRAPH_DIR + filename, 'rb') as f:
+        graphs.append({"filename":filename, "imagefile" : f.read()})   
+    
+    graph_col.insert_Many(bson.BSON.encode(graphs))
+    return filename
+
+def countries_rank_graph(data, countries,filename,title):
+    df = pd.DataFrame(data)
+    df['weekly_rank'] = df['weekly_rank'].astype(int)
+    
+    serieses = []
+    
+    for country in countries:
+        data = df.loc[df['country_name'] == country, ['week','weekly_rank']].set_index(['week'])
+        data.columns = [country]
+        serieses.append(data)
+        # print(data.tail())
+    
+    graph_df = pd.concat(serieses,axis=1).sort_index().fillna(11)
+    graph_df.plot(kind='line', figsize=(10,6),title=title, legend=True,marker="." , rot=15)
+    
+    plt.xlabel('date')
+    plt.ylabel('Ranking')
+    plt.yticks(range(1,11,1))
+    plt.ylim(top=10)
+    plt.gca().invert_yaxis()
+
+    plt.savefig(filename, dpi=400, bbox_inches='tight')
+    print(filename + 'Saved...')
+    plt.cla()
+    return filename
+
+def first_count_graph(data,filename,title):
+    df = pd.DataFrame(data)
+    place = df.groupby('country_name')['weekly_rank'].count()
+    # 색깔 적용하는 코드 만들어야됨
+    colors = ['r','g','b']
+
+    place.plot(kind='bar', figsize=(10,6),title=title, rot=15, color=colors)
+    
+    plt.xlabel('Country')
+    plt.ylabel('Count')
+    
+    plt.savefig(filename, dpi=400, bbox_inches='tight')
+    print(filename + 'Saved...')
+    plt.cla()
+    return
+# return 안해주면 해줄때 까지 
 
 # df['cumulative_weeks_in_top_10'] = df['cumulative_weeks_in_top_10'].astype('int')
 
